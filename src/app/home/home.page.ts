59864416d0494f8a3eb89ceec0c83f03161ecc3c
Senthil Kumar <senthilssk004@gmail.com>
2023-04-18 08:37:28 +0530

    home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,5 +9,70 @@ import { IonicModule } from '@ionic/angular';
   imports: [IonicModule],
 })
 export class HomePage {
-  constructor() {}
+
+  is_loading: boolean = false;
+  background_tracking: boolean = false;
+
+  constructor(
+    // private store: Store,
+    // private geolocationService: BackgroundGeolocationService,
+    // private alertController: AlertController,
+    // public loadingController: LoadingController,
+    // private launchNavigator: LaunchNavigator,
+  ) {}
+
+
+  // StopBackgroundGeolocation() {
+
+  //   if (this.vehicle._id) {
+  //     let formData = {
+  //       background_tracking: false
+  //     }
+
+  //     this.vehicleService.save(formData, this.vehicle._id).subscribe((res) => {
+
+  //       if (formData.background_tracking == false) {
+
+  //         this.store.dispatch(new GET_APPDATA({ user_id: this.user.ID })).subscribe(() => {
+
+  //           this.user = this.store.selectSnapshot(AuthState);
+  //           let appState = this.store.selectSnapshot(AppState);
+  //           this.vehicle = appState.vehicle;
+  //           this.activeLoad = appState.load;
+
+  //         });
+  //       }
+  //     });
+  //   }
+
+  //   this.geolocationService.stopBackgroundTracking();
+  // }
+
+  // StartBackgroundGeolocation() {
+
+  //   if (this.vehicle._id) {
+  //     let formData = {
+  //       background_tracking: true
+  //     }
+
+  //     this.vehicleService.save(formData, this.vehicle._id).subscribe((res) => {
+  //       console.log(res);
+
+  //       if (formData.background_tracking == true) {
+
+  //         this.store.dispatch(new GET_APPDATA({ user_id: this.user.ID })).subscribe(() => {
+
+  //           this.user = this.store.selectSnapshot(AuthState);
+  //           let appState = this.store.selectSnapshot(AppState);
+  //           this.vehicle = appState.vehicle;
+  //           this.activeLoad = appState.load;
+
+  //         });
+  //       }
+  //     });
+  //   }
+
+  //   this.geolocationService.startBackgroundTracking();
+
+  // }
 }
